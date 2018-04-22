5c8e1aa61205147bc7081407d4553b2d19e917d9
driess420 <38596619+driess420@users.noreply.github.com>
2018-04-22 02:54:26 -0700

    Add files via upload

--- percent.java
+++ percent.java
@@ -1,23 +1,31 @@
 package com.example.nevillevarghese.hackathoncode;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class percent extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
+    private String mTextMessage;
+    private double percentage;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_percent);
+        mTextMessage = team.teamName;
+        TextView myAwesomeTextView = (TextView)findViewById(R.id.Team);
+        myAwesomeTextView.setText(mTextMessage);
+    }
 
+    public void sendMessage(View view) {
+        Intent in = new Intent(this, team.class);
+        startActivity(in);
     }
 
 }
--- team.java
+++ team.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class team extends AppCompatActivity {
-    public String teamName;
+    public static String teamName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,43 +14,75 @@ public class team extends AppCompatActivity {
     }
 
     public void sendMessage(View view){
+        int id = view.getId();
+        switch (id) {
+            case R.id.KC: teamName = "Kansas City Royals";
+            break;
+            case R.id.NY: teamName = "New York Yankees";
+                break;
+            case R.id.H: teamName = "Houston Astros";
+                break;
+            case R.id.LS: teamName = "St.Louis Cardinals";
+                break;
+            case R.id.NY2: teamName = "New York Mets";
+                break;
+            case R.id.SF: teamName = "San Francisco Giants";
+                break;
+            case R.id.Sox: teamName = "Chicago White Sox";
+                break;
+            case R.id.Bird: teamName = "Toronto BlueJays";
+                break;
+            case R.id.S: teamName = "Seattle Mariners";
+                break;
+            case R.id.C: teamName = "Chicago Cubs";
+                break;
+            case R.id.M: teamName = "Miami Marlins";
+                break;
+            case R.id.A: teamName = "Arizona Diamondbacks";
+                break;
+            case R.id.TC: teamName = "Minnesota Twins";
+                break;
+            case R.id.B: teamName = "Boston Red Sox";
+                break;
+            case R.id.T: teamName = "Texas Rangers";
+                break;
+            case R.id.P: teamName = "Pittsburgh Pirates";
+                break;
+            case R.id.A2: teamName = "Atlanta Braves";
+                break;
+            case R.id.LA: teamName = "Los Angeles Dodgers";
+                break;
+            case R.id.C2: teamName = "Cleveland Indians";
+                break;
+            case R.id.TB: teamName = "Tampa Bay Rays";
+                break;
+            case R.id.A3: teamName = "Los Angeles Angels";
+                break;
+            case R.id.M2: teamName = "Milwaukee Brewers";
+                break;
+            case R.id.P2: teamName = "Philadelphia Phillies";
+                break;
+            case R.id.SD: teamName = "San Diego Padres";
+                break;
+            case R.id.D: teamName = "Detroit Tigers";
+                break;
+            case R.id.O: teamName = "Baltimore Orioles";
+                break;
+            case R.id.A4: teamName = "Oakland Athletics";
+                break;
+            case R.id.C3: teamName = "Cincinatti Reds";
+                break;
+            case R.id.W: teamName = "Washington Nationals";
+                break;
+            case R.id.CR: teamName = "Colorado Rockies";
+                break;
+        }
         Intent in = new Intent(this, percent.class);
         startActivity(in);
-        switch (view.getId()) {
-            case R.id.KC: teamName = "Kansas City Royals";
-            case R.id.NY: teamName = "New York Yankees";
-            case R.id.H: teamName = "Houston Astros";
-            case R.id.LS: teamName = "St.Louis Cardinals";
-            case R.id.NY2: teamName = "New York Mets";
-            case R.id.SF: teamName = "San Francisco Giants";
-            case R.id.Sox: teamName = "Chicago White Sox";
-            case R.id.Bird: teamName = "Toronto BlueJays";
-            case R.id.S: teamName = "Seattle Mariners";
-            case R.id.C: teamName = "Chicago Cubs";
-            case R.id.M: teamName = "Miami Marlins";
-            case R.id.A: teamName = "Arizona Diamondbacks";
-            case R.id.TC: teamName = "Minnesota Twins";
-            case R.id.B: teamName = "Boston Red Sox";
-            case R.id.T: teamName = "Texas Rangers";
-            case R.id.P: teamName = "Pittsburgh Pirates";
-            case R.id.A2: teamName = "Atlanta Braves";
-            case R.id.LA: teamName = "Los Angeles Dodgers";
-            case R.id.C2: teamName = "Cleveland Indians";
-            case R.id.TB: teamName = "Tampa Bay Rays";
-            case R.id.A3: teamName = "Los Angeles Angels";
-            case R.id.M2: teamName = "Milwaukee Brewers";
-            case R.id.P2: teamName = "Philadelphia Phillies";
-            case R.id.SD: teamName = "San Diego Padres";
-            case R.id.D: teamName = "Detroit Tigers";
-            case R.id.O: teamName = "Baltimore Orioles";
-            case R.id.A4: teamName = "Oakland Athletics";
-            case R.id.C3: teamName = "Cincinatti Reds";
-            case R.id.W: teamName = "Washington Nationals";
-            case R.id.CR: teamName = "Colorado Rockies";
+    }
 
-
-
-        }
+    public String getTeamName() {
+        return teamName;
     }
 
 }
