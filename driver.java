8b0f2d91592044c709f6e18059aacf5e167e223f
driess420 <38596619+driess420@users.noreply.github.com>
2018-04-22 08:32:31 -0700

    Add files via upload

--- driver.java
+++ driver.java
@@ -17,31 +17,29 @@ public class driver {
         String[] names = {"Kansas City Royals", "New York Yankees", "Houston Astros", "St.Louis Cardinals","New York Mets","San Francisco Giants","Chicago White Sox","Toronto BlueJays","Seattle Mariners","Chicago Cubs","Miami Marlins","Arizona Diamondbacks","Minnesota Twins","Boston Red Sox","Texas Rangers","Pittsburgh Pirates","Atlanta Braves","Los Angeles Dodgers","Cleveland Indians","Tampa Bay Rays","Los Angeles Angels","Milwaukee Brewers","Philadelphia Phillies","San Diego Padres","Detroit Tigers","Baltimore Orioles","Oakland Athletics","Cincinatti Reds","Washington Nationals","Colorado Rockies"};
         String[] namesAbbrev = {"KCR","NYY","HOU","STL","NYM","SFG","CHW","TOR","SEA","CHC","MIA","ARI","MIN","BOS","TEX","PIT","ATL","LAD","CLE","TBR","LAA","MIL","PHI","SDP","DET","BAL","OAK","CIN","WSN","COL"};
         ArrayList<Double> percentages = new ArrayList<>();
-        String command = "/Library/Frameworks/Python.framework/Versions/3.6/bin/python3 program.py";
+        String command = "C:\\Users\\devin.LAPTOP\\AppData\\Local\\Programs\\Python\\Python36\\python.exe C:\\Users\\devin.LAPTOP\\HackathonCode\\app\\src\\script.py";
 
         Process proc = Runtime.getRuntime().exec(command);
 
-        /*
-        // Read the output
 
+        //Read the output
         BufferedReader reader =
                 new BufferedReader(new InputStreamReader(proc.getInputStream()));
-
         String line = "";
         while((line = reader.readLine()) != null) {
             System.out.print(line + "\n");
         }
-        */
 
-        Scanner in = new Scanner(new InputStreamReader(proc.getInputStream()));
-        while(in.hasNextDouble()){
-            percentages.add(in.nextDouble());
-        }
+
+//        Scanner in = new Scanner(new InputStreamReader(proc.getInputStream()));
+//        while(in.hasNextDouble()){
+//            percentages.add(in.nextDouble());
+//        }
 
         HashMap<String, Double> hm = new HashMap<>();
-        for(int i = 0 ; i < names.length;i++) {
-            hm.put(namesAbbrev[i], percentages.get(i));
-        }
+//        for(int i = 0 ; i < names.length;i++) {
+//            hm.put(namesAbbrev[i], percentages.get(i));
+//        }
 
         proc.waitFor();
 
--- percent.java
+++ percent.java
@@ -8,10 +8,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class percent extends AppCompatActivity {
 
     private String mTextMessage;
     private double percentage;
+    private int pos;
 
 
     @Override
@@ -19,8 +26,42 @@ public class percent extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_percent);
         mTextMessage = team.teamName;
+        pos = team.position;
+
+        Scanner scanner = new Scanner("");
+            scanner = new Scanner("0.162475   0.01328746 0.01328746 0.26926326 0.15118366 0.01328746\n" +
+                    " 0.00115643 0.162475   0.20115643 0.22453457 0.00115643 0.30115643\n" +
+                    " 0.00115643 0.00115643 0.12045028 0.24897902 0.00115643 0.162475\n" +
+                    " 0.00115643 0.00115643 0.01328746 0.00115643 0.00115643 0.03011563\n" +
+                    " 0.28987743 0.00115643 0.00115643 0.02115643 0.00115643 0.00115643");
+        double[] dubs = new double[30];
+        for (int i = 0; i < dubs.length; i++) {
+            dubs[i] = scanner.nextDouble();
+        }
+        TextView perc = (TextView)findViewById(R.id.Ratio);
+        double fin = dubs[pos] * 100;
+        perc.setText(String.format("%.2f%s", fin, "%"));
         TextView myAwesomeTextView = (TextView)findViewById(R.id.Team);
         myAwesomeTextView.setText(mTextMessage);
+//        mTextMessage = team.teamName;
+//        pos = team.position;
+//
+//        try {
+//            Scanner in = new Scanner(new File("C:\\Users\\devin.LAPTOP\\Desktop\\output.txt"));
+//            double[] dubs = new double[30];
+//            for(int i = 0;i<dubs.length;i++) {
+//                String d = in.next();
+//                dubs[i]=Double.parseDouble(d);
+//                System.out.println(dubs[i]);
+//            }
+//            TextView perc = (TextView)findViewById(R.id.Ratio);
+//            double fin = dubs[pos] * 100;
+//            perc.setText(String.format("%.2f", fin));
+//            TextView myAwesomeTextView = (TextView)findViewById(R.id.Team);
+//            myAwesomeTextView.setText(mTextMessage);
+//        } catch(FileNotFoundException er) {
+//            System.out.println("BRUHHHH");
+//        }
     }
 
     public void sendMessage(View view) {
