4f754c68224f2bd849b7062b3455e61f18fade80
darkshire3 <alexdong66@gmail.com>
2018-04-22 02:58:04 -0700

    Updates for ML

--- ML Backend/program.py
+++ ML Backend/program.py
@@ -9,16 +9,16 @@ import glob
 import sklearn.ensemble
 #from sklearn.metrics import mean_absolute_error as mae
 
-print(os.listdir('/Users/mainTriad/Downloads/Predictions/input/baseball'))
+#print(os.listdir('/Documents/GitHub/TinoHacksSmartDubs/input/baseball/team.csv'))
 
-print (glob.glob('/Users/mainTriad/Downloads/Predictions/input/baseball/*team.csv'))
+#print (glob.glob('/Documents/GitHub/TinoHacksSmartDubs/input/baseball/team.csv'))
 
 #print( df.info() )
 
-df = pd.read_csv('/Users/mainTriad/Downloads/Predictions/input/baseball/team.csv')
-df = df.dropna(axis=0);
+df = pd.read_csv('Documents/GitHub/TinoHacksSmartDubs/input/baseball/team.csv')
+df = df.dropna(axis=0)
 
-redap = pd.read_csv('/Users/mainTriad/Downloads/Predictions/input/baseball/team.csv')
+redap = pd.read_csv('Documents/GitHub/TinoHacksSmartDubs/input/baseball/team.csv')
 redap.drop( ['year', 'attendance', 'ipouts', 'fp', 'e', 'dp', 'soa', 'ab', 'ppf', 'bpf', 'bba', 'cg', 'sho', 'sv', 'ha', 'hra', 'hbp', 'sf', 'ra', 'er', 'era', 'hr', 'bb', 'so', 'sb', 'cs'], axis = 1, inplace = True )
 
 #redap = redap[df.team_id_br == 'BOS']
@@ -47,18 +47,30 @@ model = sklearn.ensemble.RandomForestRegressor(max_depth=2, random_state=0)
 model.fit(X, y)
 r = 6
 
-test = pd.read_csv('/Users/mainTriad/Downloads/Predictions/input/baseball/team_half.csv')
+test = pd.read_csv('Documents/GitHub/TinoHacksSmartDubs/input/baseball/team.csv')
+test = test.dropna(axis = 0)
 
-print(test.head(10))
+#print(test.head(10))
 
-test = df.head(100)
+# input = input("your team? ")
 
-input
+# test = test[test.team_id == input]
 
-test = test.team_id == 'BOS'
+#print( test )
 
-y = test.lg_win
-X = test.loc[:,['w','l']]
+y = test.loc[:,['lg_win']]
+#X = test.loc[:,['w','l']]
+
+#print(model.predict( X ))
+
+df = pd.read_csv('Documents/GitHub/TinoHacksSmartDubs/input/season.csv')
+
+df = df[ df.w * 7.4 ]
+
+print( df.head(10) )
+
+#y = df.loc[:,['lg_win']]
+X = df.loc[:,['w', 'l']]
 
 print(model.predict( X ))
 
